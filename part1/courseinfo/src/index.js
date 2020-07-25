563f161e48591156e652511d1af0cf7d54d2fc72
The-East-Wind <hariatul1998@gmail.com>
2020-07-25 22:24:31 +0530

    completed exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -29,26 +29,28 @@ const Total = (props) => (
 );
 
 const App = () => {
-  const course = 'Half stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total exercises={parts.map(part=>part.exercises)}/>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map(part=>part.exercises)} />
     </div>
   )
 };

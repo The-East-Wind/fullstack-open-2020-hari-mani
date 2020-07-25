f7fb72986a99324ae359fd9b22a7f97bdf8971cd
The-East-Wind <hariatul1998@gmail.com>
2020-07-25 22:05:05 +0530

    completed exercise 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+    {props.part.name} {props.part.exercises}
   </p>
 );
 
@@ -12,37 +12,43 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 );
 
-const Content = (props) => (
-  <div>
-    <Part part={props.part1.name} exercises={props.part1.exercises}/>
-    <Part part={props.part2.name} exercises={props.part2.exercises}/>
-    <Part part={props.part3.name} exercises={props.part3.exercises}/>
-  </div>
-);
+const Content = (props) => {
+  const[part1,part2,part3] = props.parts;
+  console.log(part1,part2,part3);
+  return (
+    <div>
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+    </div>
+  );
+}
 
 const Total = (props) => (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises {props.exercises.reduce((a,b)=>a+b,0)}</p>
 );
 
 const App = () => {
   const course = 'Half stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Content parts={parts} />
+      <Total exercises={parts.map(part=>part.exercises)}/>
     </div>
   )
 };
